7d1a0219403ad22f48e937281ce8b23ce7c163aa
hyo-limilimee <80194553+hyo-limilimee@users.noreply.github.com>
2021-06-29 17:30:07 +0900

    constant_datatype

---
+++ 01_constant2.c
@@ -0,0 +1,11 @@
+/*상수의 종류*/
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", 10);   //정수형 수치 상수
+	printf("%d %d\n", 10, 20);
+	printf("%lf\n", 3.14);
+	printf("%c\n", 'a');
+	printf("%s\n", "sky");
+	printf("%d\n", "10");
+}
