60a6814302aab2d0eab1cf887769a87b57f5b9ba
hyo-limilimee <80194553+hyo-limilimee@users.noreply.github.com>
2021-06-30 00:53:46 +0900

    Update 01_constant2.c

--- 01_constant2.c
+++ 01_constant2.c
@@ -1,8 +1,8 @@
-/*상수의 종류*/
+/*type of constant*/
 #include <stdio.h>
 int main()
 {
-	printf("%d\n", 10);   //정수형 수치 상수
+	printf("%d\n", 10);   
 	printf("%d %d\n", 10, 20);
 	printf("%lf\n", 3.14);
 	printf("%c\n", 'a');
