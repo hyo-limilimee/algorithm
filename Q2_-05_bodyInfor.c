417e15a46a2e0c77ad40dd4876b71a6466a1a2df
hyo-limilimee <80194553+hyo-limilimee@users.noreply.github.com>
2021-06-30 00:57:24 +0900

    use datatype
    practice data type

---
+++ Q2_-05_bodyInfor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf("성명 : %s\n","홍길동");
+	printf("성별 : %c\n", 'M');
+	printf("나이 : %d\n", 20);
+	printf("키 : %f\n", 175.5);
+	printf("몸무게 : %f\n", 52.4);
+	return 0;
+}
