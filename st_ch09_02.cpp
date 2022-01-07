9a6c20069e9ef44d8fff2a2e7d76aaf1fadda9ff
hyo-limilimee <80194553+hyo-limilimee@users.noreply.github.com>
2022-01-08 02:58:41 +0900

    Add files via upload

---
+++ st_ch09_02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+class Pizza {
+	int radius;
+public:
+	Pizza(int r = 0) :radius{ r } { }
+	~Pizza() { }
+
+	void setRadius(int r) { radius = r;}
+	void print()
+	{
+		cout << "Pizza(" << radius << ")" << endl;
+	}
+};
+
+void upgrade(Pizza& pizza)
+{
+	pizza.setRadius(20);
+}
+
+int main()
+{
+	Pizza obj(10);
+	upgrade(obj);
+	obj.print();
+	return 0;
+}
