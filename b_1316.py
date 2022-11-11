f754058e4e845eb7ae8a86b6b3d597655579c134
hyo-limilimee <80194553+hyo-limilimee@users.noreply.github.com>
2022-11-12 02:31:36 +0900

    Add files via upload

---
+++ b_1316.py
@@ -0,0 +1,12 @@
+N = int(input())
+result = N
+
+for i in range(N):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] != word[j+1]:
+            if word[j] in word[j+1:]:
+                result -= 1
+                break
+
+print(result)
