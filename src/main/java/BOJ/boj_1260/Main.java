c2634d74dfbbc73c59ad4e5d6acd05d6789ba915
hyo-limilimee <1001lily@naver.com>
2023-12-29 21:28:56 +0900

    feat: BOJ 1260 solved

---
+++ src/main/java/BOJ/boj_1260/Main.java
@@ -0,0 +1,82 @@
+package BOJ.boj_1260;
+
+import java.util.*;
+
+public class Main {
+    static int N, M, V; // 정점 개수, 간선 개수, 시작 정점
+    static ArrayList<ArrayList<Integer>> graph; // 그래프를 인접 리스트로 표현
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
+
+        // 그래프 초기화
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // 간선 정보 입력받기
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a); // 양방향 간선이므로 양쪽에 모두 추가
+        }
+
+        // 그래프 정렬 (작은 정점 번호 먼저 방문하기 위해)
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        // DFS 수행 및 출력
+        boolean[] visitedDFS = new boolean[N + 1];
+        dfs(V, visitedDFS);
+        System.out.println();
+
+        // BFS 수행 및 출력
+        boolean[] visitedBFS = new boolean[N + 1];
+        bfs(V, visitedBFS);
+
+        sc.close();
+    }
+
+    // DFS 구현
+    private static void dfs(int v, boolean[] visited) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        for (int next : graph.get(v)) {
+            if (!visited[next]) {
+                dfs(next, visited);
+            }
+        }
+    }
+
+    // BFS 구현
+    private static void bfs(int start, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start); // 큐에 원소 추가
+        visited[start] = true;
+
+        // 큐에 원소가 있는 동안 방문
+        while (!queue.isEmpty()) {
+            int current = queue.poll(); // 큐의 맨 앞 원소 제거
+            System.out.print(current + " ");    // 제거한 원소 출력
+
+            // 현재 노드에 연결된 모든 노드에 대해 반복
+            // graph.get(current)는 현재 노드와 연결된 모든 이웃 노드들의 리스트를 반환
+            for (int next : graph.get(current)) {
+                // 이웃 노드가 방문 되지 않은 경우
+                if (!visited[next]) {
+                    queue.offer(next);  // 이웃 노드를 큐에 추가
+                    visited[next] = true;   //이웃 노드 방문 했으므로 true
+                }
+            }
+        }
+    }
+}
--- src/main/java/SWEA/swea_19003.java
+++ src/main/java/SWEA/swea_19003.java
@@ -1,4 +1,52 @@
-package SWEA;
-
-public class swea_19003 {
-}
+//package SWEA;
+//
+//import java.util.HashMap;
+//import java.util.Scanner;
+//
+//public class swea_19003 {
+//    public static void main(String args[]) throws Exception {
+//        Scanner sc = new Scanner(System.in);
+//        int T;
+//        T = sc.nextInt();
+//
+//        for (int test_case = 1; test_case <= T; test_case++) {
+//            int N = sc.nextInt();
+//            int M = sc.nextInt();
+//            sc.nextLine();
+//
+//            // 문자열을 저장할 배열
+//            String[] strings = new String[N];
+//            for (int i = 0; i < N; i++) {
+//                strings[i] = sc.nextLine();
+//            }
+//
+//            // 문자열을 길이별로 분류하여 저장할 HashMap
+//            HashMap<Integer, Integer> lengthCountMap = new HashMap<>();
+//
+//            // 각 문자열의 길이를 세어 HashMap에 저장
+//            for (String str : strings) {
+//                int len = str.length();
+//                lengthCountMap.put(len, lengthCountMap.getOrDefault(len, 0) + 1);
+//            }
+//
+//            int result = 0;
+//            boolean oddLengthUsed = false;
+//
+//            // 길이별로 팰린드롬을 만들기
+//            for (int len : lengthCountMap.keySet()) {
+//                int count = lengthCountMap.get(len);
+//
+//                // 길이가 홀수인 경우 한 번만 사용
+//                if (len % 2 == 1 && !oddLengthUsed) {
+//                    result += 1;
+//                    oddLengthUsed = true;
+//                }
+//
+//                // 짝수인 경우 모두 사용
+//                result += count / 2 * 2;
+//            }
+//
+//            System.out.println("#" + test_case + " " + result);
+//        }
+//    }
+//}
