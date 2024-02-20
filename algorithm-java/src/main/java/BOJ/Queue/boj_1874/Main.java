9b4b3d96aa9ba82a5e2e72057f6e202e35fa88f1
hyo-limilimee <1001lily@naver.com>
2024-02-20 18:35:49 +0900

    feat: solve BOJ 1874

---
+++ algorithm-java/src/main/java/BOJ/Queue/boj_1874/Main.java
@@ -0,0 +1,44 @@
+package BOJ.Queue.boj_1874;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        int start = 0;
+
+        // n개의 숫자에 대해서 반복
+        for (int i = 0; i < n; i++) {
+            // 숫자 불러오기
+            int x = Integer.parseInt(br.readLine());
+
+            if(x > start) {
+                for (int j = start + 1; j <= x; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
+                }
+                start = x;
+            }
+
+            // 불가능한 경우
+            else if(stack.peek()!=x){
+                System.out.println("NO");
+                return;
+            }
+
+                stack.pop();
+                sb.append('-').append('\n');
+
+        }
+        System.out.println(sb);
+    }
+}
