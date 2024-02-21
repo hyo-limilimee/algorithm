38b72b35557215d9386f659dbc5770bfa4d5c86d
hyo-limilimee <1001lily@naver.com>
2024-02-21 18:16:43 +0900

    feat: solve BOJ 14584

---
+++ algorithm-java/src/main/java/BOJ/String/boj_14584/Main.java
@@ -0,0 +1,33 @@
+package BOJ.String.boj_14584;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String cipher = sc.next();
+
+        int n = sc.nextInt();
+        String s[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+
+        for (int i = 0; i < 26; i++) {
+            String temp = "";
+            for (int j = 0; j < cipher.length(); j++) {
+                temp += (char) ((cipher.charAt(j) - 'a' + i) % 26 + 'a');
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (temp.contains(s[j])) {
+                    System.out.println(temp);
+                    return;
+                }
+            }
+        }
+    }
+}
