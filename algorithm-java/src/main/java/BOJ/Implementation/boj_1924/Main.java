82276a83dbe6d4aaa8b14b8d8e0989622f449740
hyo-limilimee <1001lily@naver.com>
2024-02-21 17:52:59 +0900

    feat: solve BOJ 1924

---
+++ algorithm-java/src/main/java/BOJ/Implementation/boj_1924/Main.java
@@ -0,0 +1,26 @@
+package BOJ.Implementation.boj_1924;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        int[] monthOfDay = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int n = 0;
+
+        for (int i = 0; i < month; i++) {
+            n += monthOfDay[i];
+        }
+
+        n += day - 1;
+        System.out.println(days[n % 7]);
+    }
+}
