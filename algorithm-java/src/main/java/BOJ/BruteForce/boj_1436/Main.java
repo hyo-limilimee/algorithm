47ac375decb3c57bade3a39831f5212b81543d8b
hyo-limilimee <1001lily@naver.com>
2024-04-03 23:44:06 +0900

    feat: solve BOJ 1436

---
+++ algorithm-java/src/main/java/BOJ/BruteForce/boj_1436/Main.java
@@ -0,0 +1,24 @@
+package BOJ.BruteForce.boj_1436;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N){
+            num ++;
+
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
