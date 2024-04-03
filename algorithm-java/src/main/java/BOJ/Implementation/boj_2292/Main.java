e1d197ee4f8a322ad518cbd3afd73b7b992e249f
hyo-limilimee <1001lily@naver.com>
2024-04-04 00:01:26 +0900

    feat: solve BOJ 2292

--- algorithm-java/src/main/java/BOJ/Implementation/boj_2292/Main.java
+++ algorithm-java/src/main/java/BOJ/Implementation/boj_2292/Main.java
@@ -1,7 +1,6 @@
-package BOJ.boj_2292;
+package BOJ.Implementation.boj_2292;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
