9bbd468edf8ac0c2c7bb816acce56927a28e2917
hyo-limilimee <1001lily@naver.com>
2024-05-23 03:29:03 +0900

    feat: solve BOJ 2504

---
+++ algorithm-java/src/main/java/BOJ/Stack/boj_2504/Main.java
@@ -0,0 +1,55 @@
+package BOJ.Stack.boj_2504;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        int temp = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if(c == '('){
+                stack.push(c);
+                temp *= 2;
+            }
+
+            else if(c == '['){
+                stack.push(c);
+                temp *= 3;
+            }
+
+           else if(c == ')'){
+               if(stack.isEmpty() || stack.peek() != '('){
+                   result = 0;
+                   break;
+               }
+               else if(str.charAt(i-1) == '('){
+                   result += temp;
+               }
+               stack.pop();
+               temp /= 2;
+            }
+           else if(c == ']'){
+                if(stack.isEmpty() || stack.peek() != '['){
+                    result = 0;
+                    break;
+                }
+                else if(str.charAt(i-1) == '['){
+                    result += temp;
+                }
+                stack.pop();
+                temp /= 3;
+            }
+        }
+        System.out.println(result);
+    }
+}
