4067a08dd65b577b21d65df1df7f4bf8a2778197
hyo-limilimee <1001lily@naver.com>
2024-05-23 20:35:31 +0900

    feat: solve BOJ 15561

---
+++ algorithm-java/src/main/java/BOJ/BruteForce/boj_15661/Main.java
@@ -0,0 +1,17 @@
+package BOJ.BruteForce.boj_15661;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // N명 : 축구를 하기 위해 모인 사람
+        // 스타트 & 링크 팀으로 나눠야함 (인원수는 달라도 되지만 한명 이상)
+
+        // 능력치: Sij + Sji
+
+        // 능력치의 차이 최소화
+    }
+}
