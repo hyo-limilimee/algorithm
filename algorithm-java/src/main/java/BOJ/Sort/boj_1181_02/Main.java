44e27a71f86775c1495d3aaac926ad6f4b1eca08
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-01-29 21:29:00 +0900

    feat: solve BOJ 1676

---
+++ algorithm-java/src/main/java/BOJ/Sort/boj_1181_02/Main.java
@@ -0,0 +1,44 @@
+package BOJ.Sort.boj_1181_02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        // 길이가 짧은 순서
+        // 길이가 같으면 사전 순
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        // 중복되지 않는 단어만 출력
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append('\n');
+
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
