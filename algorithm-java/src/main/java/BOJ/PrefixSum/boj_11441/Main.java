490669c8ecaf327258b18c1eae5ebe803e6677a0
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-03-21 19:33:18 +0900

    feat: solve BOJ 11441

---
+++ algorithm-java/src/main/java/BOJ/PrefixSum/boj_11441/Main.java
@@ -0,0 +1,35 @@
+package BOJ.PrefixSum.boj_11441;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] A = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] prefixSum = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] = prefixSum[i - 1] + A[i];
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            sb.append(prefixSum[right] - prefixSum[left - 1]).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
