a9a348273d10c1387eac1daf2a92ce1be74864b6
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-04-08 16:53:05 +0900

    feat: solve BOJ 1182

--- algorithm-java/src/main/java/BOJ/DFS/boj_1182_2/Main.java
+++ algorithm-java/src/main/java/BOJ/DFS/boj_1182_2/Main.java
@@ -7,8 +7,6 @@ public class Main {
     static int N;
     static int S;
     static int[] arr;
-    static int[] picked;
-    static int pickedNum;
     static int count = 0;
 
     public static void main(String[] args) throws Exception {
@@ -23,33 +21,22 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++) {
-            picked = new int[i];
-            pickedNum = i;
-            makeSum(0, 0);
+        makeSum(0, 0);
+        if (S == 0) {
+            count--;
         }
         System.out.println(count);
     }
 
-    private static void makeSum(int index, int depth) {
-        if (depth == pickedNum) {
-            if (calculateSum() == S) {
+    private static void makeSum(int index, int sum) {
+        if (index == N) {
+            if (sum == S) {
                 count++;
             }
             return;
         }
 
-        for (int i = index; i < N; i++) {
-            picked[depth] = arr[i];
-            makeSum(i + 1, depth + 1);
-        }
-    }
-
-    private static int calculateSum() {
-        int sum = 0;
-        for (int i = 0; i < picked.length; i++) {
-            sum += picked[i];
-        }
-        return sum;
+        makeSum(index + 1, sum + arr[index]); // 현재 수 포함
+        makeSum(index + 1, sum);            // 현재 수 미포함
     }
 }
