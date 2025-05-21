7ef4f42d1ff4248665762160cd00d9fd59c9d1c3
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-21 15:35:06 +0900

    feat: solve BOJ 9375

---
+++ algorithm-java/src/main/java/BOJ/boj_9375/Main.java
@@ -0,0 +1,31 @@
+package BOJ.boj_9375;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < t; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int cnt = 1;
+            for (int value : map.values()) {
+                cnt *= (value + 1);
+            }
+            System.out.println(cnt - 1);
+        }
+    }
+}
