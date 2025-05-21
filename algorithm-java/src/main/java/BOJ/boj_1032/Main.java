937412e60b5d5868ca15d136ff340735665156ea
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-21 22:59:07 +0900

    feat: solve BOJ 1032

---
+++ algorithm-java/src/main/java/BOJ/boj_1032/Main.java
@@ -0,0 +1,36 @@
+package BOJ.boj_1032;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] names = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            names[i] = br.readLine();
+        }
+
+        int length = names[0].length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            boolean flag = true;
+
+            for(int j = 1; j < N; j++){
+                if(names[j].charAt(i) != names[j-1].charAt(i)){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                sb.append(names[0].charAt(i));
+            }else{
+                sb.append("?");
+            }
+        }
+        System.out.println(sb);
+    }
+}
