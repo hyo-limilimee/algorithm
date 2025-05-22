330991a1d42d712783eaf03915609baf840eca97
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-22 18:11:06 +0900

    feat: solve BOJ 11047

---
+++ algorithm-java/src/main/java/BOJ/Greedy/boj_11047_2/Main.java
@@ -0,0 +1,26 @@
+package BOJ.Greedy.boj_11047_2;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            cnt += K / A[i];
+            K = K % A[i];
+        }
+        System.out.println(cnt);
+    }
+}
