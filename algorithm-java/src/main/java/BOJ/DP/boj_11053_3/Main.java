e4ed257e40bbeb354b40e0e6507fe8ecfccf5262
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-22 18:34:39 +0900

    feat: solve BOJ 11053

---
+++ algorithm-java/src/main/java/BOJ/DP/boj_11053_3/Main.java
@@ -0,0 +1,33 @@
+package BOJ.DP.boj_11053_3;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i = 0; i < N; i++){
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
