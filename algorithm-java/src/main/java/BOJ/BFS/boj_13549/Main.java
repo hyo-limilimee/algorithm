9051e1a0821ee20f94676f9a88dabab8f47b812f
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-05-23 21:06:46 +0900

    feat: solve BOJ 13549

---
+++ algorithm-java/src/main/java/BOJ/BFS/boj_13549/Main.java
@@ -0,0 +1,45 @@
+package BOJ.BFS.boj_13549;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[] dist;
+    static int MAX = 1000001;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dist = new int[MAX];
+        Arrays.fill(dist, -1);
+
+        bfs();
+
+        System.out.println(dist[K]);
+    }
+
+    private static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+        dist[N] = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int next : new int[]{current * 2, current - 1, current + 1}) {
+                if (next >= 0 && next < MAX) {
+                    if (dist[next] == -1) {
+                        if (next == current * 2) {
+                            dist[next] = dist[current];
+                        } else {
+                            dist[next] = dist[current] + 1;
+                        }
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+    }
+}
