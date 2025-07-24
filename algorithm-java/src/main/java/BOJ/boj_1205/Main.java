d3690b2181b0a417d8297c31a218f4d5876ec4fe
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-07-24 23:33:47 +0900

    feat: solve BOJ 1205

---
+++ algorithm-java/src/main/java/BOJ/boj_1205/Main.java
@@ -0,0 +1,38 @@
+package BOJ.boj_1205;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int newScore = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[] scores = new int[N];
+        if (N > 0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                scores[i] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        if (N == P && newScore <= scores[N - 1]) {
+            System.out.println(-1);
+            return;
+        }
+
+        int rank = 1;
+        for (int i = 0; i < N; i++) {
+            if (scores[i] > newScore) {
+                rank++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(rank);
+    }
+}
