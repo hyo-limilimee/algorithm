c8bcf3d9cfdfe3e54a3a3174f3c4d82652b61a1d
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-08-03 13:19:48 +0900

    feat: solve BOJ 11742

---
+++ algorithm-java/src/main/java/BOJ/DFS/boj_11742/Main.java
@@ -0,0 +1,49 @@
+package BOJ.DFS.boj_11742;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static List<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
