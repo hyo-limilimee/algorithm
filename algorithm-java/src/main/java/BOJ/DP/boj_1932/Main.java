930a03c75f9c2c069f014f2f286b739d095b5873
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-08-05 19:14:26 +0900

    feat: solve BOJ 1932

---
+++ algorithm-java/src/main/java/BOJ/DP/boj_1932/Main.java
@@ -0,0 +1,31 @@
+package BOJ.DP.boj_1932;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] += Math.max(arr[i + 1][j], arr[i + 1][j + 1]);
+            }
+        }
+
+        System.out.println(arr[0][0]);
+    }
+}
