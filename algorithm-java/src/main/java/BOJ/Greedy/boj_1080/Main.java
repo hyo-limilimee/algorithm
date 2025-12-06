e804ebe928ff5534bebf41a43ab5087b61e98674
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-06 21:23:50 +0900

    feat: solve BOJ 1080

---
+++ algorithm-java/src/main/java/BOJ/Greedy/boj_1080/Main.java
@@ -0,0 +1,62 @@
+package BOJ.Greedy.boj_1080;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[][] A, B;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+        B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= M - 3; j++) {
+                if (A[i][j] != B[i][j]) {
+                    flip(i, j);
+                    count++;
+                }
+            }
+        }
+
+        if (isSame()) System.out.println(count);
+        else System.out.println(-1);
+    }
+
+    static void flip(int x, int y) {
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                A[i][j] = 1 - A[i][j];
+            }
+        }
+    }
+
+    static boolean isSame() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
