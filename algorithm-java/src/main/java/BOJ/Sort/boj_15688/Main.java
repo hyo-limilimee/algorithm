2c6637f0554e9ee47b2a323e6b10cc82e645e019
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-12-28 21:14:04 +0900

    feat: solve BOJ 15688

---
+++ algorithm-java/src/main/java/BOJ/Sort/boj_15688/Main.java
@@ -0,0 +1,21 @@
+package BOJ.Sort.boj_15688;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) sb.append(arr[i] + "\n");
+        System.out.println(sb);
+    }
+}
