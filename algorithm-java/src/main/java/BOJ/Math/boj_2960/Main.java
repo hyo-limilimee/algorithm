7103c829592f35d6a653d532d27635ba519feaf1
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-04 22:01:10 +0900

    feat: solve BOJ 2960

---
+++ algorithm-java/src/main/java/BOJ/Math/boj_2960/Main.java
@@ -0,0 +1,33 @@
+package BOJ.Math.boj_2960;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[] removed = new boolean[N + 1];
+        int cnt = 0;
+
+        for (int p = 2; p <= N; p++) {
+            if (removed[p]) continue;
+
+            for (int i = p; i <= N; i += p) {
+                if (removed[i]) continue;
+
+                removed[i] = true;
+                cnt++;
+
+                if (cnt == K) {
+                    System.out.println(i);
+                    return;
+                }
+            }
+        }
+    }
+}
