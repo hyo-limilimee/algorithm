7e163f106f5eb1bfe919f63446ff2bf40f690c90
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-04 21:42:29 +0900

    feat: solve BOJ 1929

---
+++ algorithm-java/src/main/java/BOJ/Math/boj_1929/Main.java
@@ -0,0 +1,26 @@
+package BOJ.Math.boj_1929;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
