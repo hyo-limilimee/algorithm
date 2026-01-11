9c826800c76ac004d868de0be33ce4e0e48c6c34
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-11 20:56:51 +0900

    feat: solve BOJ 10876

---
+++ algorithm-java/src/main/java/BOJ/Sort/boj_10876/Main.java
@@ -0,0 +1,24 @@
+package BOJ.Sort.boj_10876;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) set.add(Integer.parseInt(st.nextToken()));
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for(int num : list){
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
