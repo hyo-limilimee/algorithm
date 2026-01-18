987fe574a6691577b5b4fb4bc3787eadf84cd1bf
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-18 22:12:52 +0900

    feat: solve BOJ 33846

---
+++ algorithm-java/src/main/java/BOJ/Sort/boj_33846/Main.java
@@ -0,0 +1,27 @@
+package BOJ.Sort.boj_33846;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, 0, M);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
