953646660d0738a00881ccc0e1c72eddc68a8436
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2026-01-18 21:43:16 +0900

    feat: solve BOJ 9237

---
+++ algorithm-java/src/main/java/BOJ/Greedy/boj_9237/Main.java
@@ -0,0 +1,24 @@
+package BOJ.Greedy.boj_9237;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        int maxDay = 0;
+
+        for (int i = 0; i < N; i++) {
+            int growDay = arr[N - 1 - i];
+            int finishDay = (i + 1) + growDay;
+            maxDay = Math.max(maxDay, finishDay);
+        }
+        System.out.println(maxDay + 1);
+    }
+}
